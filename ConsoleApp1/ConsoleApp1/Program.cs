f15111aa486e1953735e8bee6a0bde08dcc25152
BugriyOleg <93713464+BugriyOleg@users.noreply.github.com>
2023-11-24 18:35:38 +0200

    Revert "Revert "Виконав реалізацію програми, залишилося реалізувати методи.""
    This reverts commit 5bb60c1b1073cc1e7c57711d0cf14dab48e5ad74.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            {
+
+                Console.Write("Введіть перший операнд: ");
+                int operandOne = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введіть другий операнд: ");
+                int operandTwo = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введіть операцію (+, -, *, /): ");
+                char operation = Convert.ToChar(Console.ReadLine());
+
+
+                switch (operation)
+                {
+                    case '+':
+                        Add(operandOne, operandTwo);
+                        break;
+                    case '-':
+                        Sub(operandOne, operandTwo);
+                        break;
+                    case '*':
+                        Mul(operandOne, operandTwo);
+                        break;
+                    case '/':
+                        Div(operandOne, operandTwo);
+                        break;
+                    default:
+                        Console.WriteLine("Невірна операція. Введіть правильний знак (+, -, *, /).");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
+            void Add(int operandOne, int operandTwo)
+            {
+                
+            }
+
+            void Sub(int operandOne, int operandTwo)
+            {
+                
+            }
+
+            void Mul(int operandOne, int operandTwo)
+            {
+                
+            }
+
+            void Div(int operandOne, int operandTwo)
+            {
+                
+            }
+        }
+    }
+    
+}
